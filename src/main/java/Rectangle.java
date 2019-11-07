54d95f739b48cc80fd572db56fb518e9f65d9365
sathyapriya <spriya2306@gmail.com>
2019-11-07 20:26:13 +0530

    Update.Included factory method and implemeted the logic for square are and perimeter

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,9 +1,22 @@
 class Rectangle {
-    int area(int length, int breadth) {
+    private int length;
+    private int breadth;
+    static final private int increaseBy2 = 2;
+
+    Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    static Rectangle createSquare(int side) {
+        return new Rectangle(side,side);
+    }
+
+    public int area() {
         return length * breadth;
     }
 
-    int perimeter(int length, int breadth) {
-        return 2 * (length + breadth);
+    public int perimeter() {
+        return increaseBy2 * (length + breadth);
     }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,39 +1,72 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RectangleTest {
     @Test
-    @DisplayName("Should area be 0 for length 0 and breadth 2")
+    @DisplayName("Should area be 0 for length 0 and breadth 0")
     void shouldBeZero() {
-        Rectangle rectangle = new Rectangle();//Arrange
-        int actualArea = rectangle.area(0, 2); //Action
+        Rectangle rectangle = new Rectangle(0, 0);//Arrange
+        int actualArea = rectangle.area(); //Action
         assertEquals(0, actualArea); //Assertion
     }
 
     @Test
     @DisplayName("calculate area for length 2 and breadth 3")
     void shouldBeSixWhenLengthTwoAndBreadthThree() {
-        Rectangle rectangle = new Rectangle();
-        assertEquals(6, rectangle.area(2, 3));
+        Rectangle rectangle = new Rectangle(2, 3);
+        assertEquals(6, rectangle.area());
     }
 
     @Test
     @DisplayName("Should perimeter be 0 for length 0 and breadth 0")
     void shouldPerimeterBeZero() {
-        Rectangle rectangle = new Rectangle();//Arrange
-        int actualArea = rectangle.perimeter(0, 0); //Action
+        Rectangle rectangle = new Rectangle(0, 0);//Arrange
+        int actualArea = rectangle.perimeter(); //Action
         assertEquals(0, actualArea); //Assertion
     }
 
     @Test
     @DisplayName("Should perimeter be 0 for length 0 and breadth 0")
     void shouldPerimeterBeTenWhenLengthTwoAndBreadthThree() {
-        Rectangle rectangle = new Rectangle();//Arrange
-        int actualArea = rectangle.perimeter(2, 3); //Action
+        Rectangle rectangle = new Rectangle(2, 3);//Arrange
+        int actualArea = rectangle.perimeter(); //Action
         assertEquals(10, actualArea); //Assertion
+    }
+
+    @Nested
+    class squareTest{
+        @Test
+        @DisplayName("Should area be 1 when side 1")
+        void shouldBeUnitArea() {
+            Rectangle rectangle = Rectangle.createSquare(0);
+            assertNotNull(rectangle);
+           // assertEquals(0, rectangle.area());
+        }
+
+        @Test
+        @DisplayName("Should area be 4 when side 2")
+        void shouldBeArea4WhenSide2() {
+            Rectangle rectangle = Rectangle.createSquare(2);
+            assertEquals(4, rectangle.area());
+        }
+
+        @Test
+        @DisplayName("Should area be 1 when side 1")
+        void shouldBeUnitPerimeter() {
+            Rectangle rectangle = Rectangle.createSquare(0);
+            assertNotNull(rectangle);
+            // assertEquals(0, rectangle.area());
+        }
+
+        @Test
+        @DisplayName("Should area be 4 when side 2")
+        void shouldBePerimeter4WhenSide2() {
+            Rectangle rectangle = Rectangle.createSquare(2);
+            assertEquals(8, rectangle.perimeter());
+        }
     }
 
 }
